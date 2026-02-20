ef2b1ca21a8e8fb8a037005d8b414a8869caf919
Thomas Q <thomasqsa@gmail.com>
2026-02-20 18:54:22 -0300

    Make entrypoint portable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 set -e
 
+if [ -f "/app/config.json" ]; then
+    CONFIG_PATH="/app/config.json"
+elif [ -f "./config.json" ]; then
+    CONFIG_PATH="./config.json"
+else
+    echo "Error: config.json not found in /app or current directory"
+    exit 1
+fi
+
 if [ -n "$CINNY_PROXY" ]; then
-    echo "Updating proxy configuration to: $CINNY_PROXY"
+    echo "Updating proxy configuration to: $CINNY_PROXY in $CONFIG_PATH"
     
     tmp=$(mktemp)
-    jq --arg proxy "$CINNY_PROXY" '.proxy = $proxy' /app/config.json > "$tmp" && mv "$tmp" /app/config.json
+    jq --arg proxy "$CINNY_PROXY" '.proxy = $proxy' "$CONFIG_PATH" > "$tmp" && mv "$tmp" "$CONFIG_PATH"
 fi
 
 exec "$@"
